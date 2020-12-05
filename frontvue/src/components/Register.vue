3223a80b586167f0044217dd6be3718324bd5796
NicolasB2 <nicolas.biojo@correo.icesi.edu.co>
2020-12-05 13:55:51 -0500

    Added register

---
+++ frontvue/src/components/Register.vue
@@ -0,0 +1,123 @@
+<template>
+ <section class="font" >
+  <div id="Login" >
+    <v-flex class="vux-center">
+      <div class="login-form">
+        <v-card elevation="10" class="form-wrapper">
+          <v-card-title>
+            <div class="display-1 mb-2"> Register</div>
+          </v-card-title>
+          <v-form v-model="valid">
+            <v-text-field
+              prepend-icon="mdi-email"
+              label="Email"
+              required
+              :rules="emailRules"
+              v-model.lazy="email"
+            ></v-text-field>
+            <v-text-field
+              prepend-icon="mdi-lock"
+              autocomplete="current-password"
+              :value="password"
+              label="Enter password"
+              :append-icon="value ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="() => (value = !value)"
+              :type="value ? 'password' : 'text'"
+              :rules="passwordRule"
+              v-model="password"
+            ></v-text-field>
+            <v-text-field
+              prepend-icon="mdi-account"
+              label="Username"
+              required
+            ></v-text-field>
+            <v-text-field
+              prepend-icon="mdi-account"
+              label="Full Name"
+              required
+            ></v-text-field>
+            <v-text-field
+              prepend-icon="mdi-phone"
+              label="PhoneNumber"
+              required
+            ></v-text-field>
+            <v-text-field
+              prepend-icon="mdi-account-box-multiple"
+              label="IdNumber"
+              required
+            ></v-text-field>
+            <v-btn :disabled="!valid" @click="verifyUser" router>Register</v-btn>
+          </v-form>
+        </v-card>
+        <v-dialog v-model="error" max-width="400px">
+          <v-alert type="error"
+            >The user email or password is incorrect</v-alert
+          >
+        </v-dialog>
+      </div>
+    </v-flex>
+  </div>
+ </section> 
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      error: false,
+      email: "",
+      password: "",
+      value: true,
+      submitted: false,
+      valid: false,
+      emailRules: [
+        (email) => !!email || "Email is required",
+        (email) =>
+          /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email) ||
+          "Email is invalid",
+      ],
+      passwordRule: [
+        (Password) => !!Password || "password is required",
+        (Password) => Password.length >= 7 || "Password is too short",
+      ],
+      rules: {
+        required: (value) => !!value || "Required.",
+        password: (value) => {
+          const pattern = /^(?=.*[a-z])/;
+          return (
+            pattern.test(value) ||
+            "Min. 8 characters with at least one capital letter, a number and a special character."
+          );
+        },
+      },
+    };
+  },
+  methods: {
+    async verifyUser() {
+
+        this.$router.push('/catalog')
+    },
+  },
+};
+</script>
+
+<style scoped>
+.form-wrapper {
+  padding-top: 100px;
+  padding-bottom: 100px;
+  padding-left: 200px;
+  padding-right: 200px;
+  border-top: 200px;
+}
+.login-form {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+.font{
+  background: #0f2032;
+  
+}
+</style>
--- frontvue/src/main.js
+++ frontvue/src/main.js
@@ -8,6 +8,7 @@ import Home from './components/Home';
 import Vehicles from './components/Vehicles';
 import DetailVehicle from './components/DetailVehicle';
 import Login from './components/Login';
+import Register from './components/Register';
 import UserDatail from './components/UserDetail';
 
 
@@ -27,6 +28,10 @@ const routes = [
   {
     path: '/login',
     component: Login
+  },
+  {
+    path: '/register',
+    component: Register
   },
   {
     path: '/userdetail',
